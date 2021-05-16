eb93fa83b943ecb432e6946e9433754cecfa8e5a
Atin Gupta <atingupta2005@gmail.com>
2021-05-16 11:57:00 +0530

    -

--- Hands-On/7-Jenkins-in-Action/Resources/pipeline/jenkins/deploy/deploy.sh
+++ Hands-On/7-Jenkins-in-Action/Resources/pipeline/jenkins/deploy/deploy.sh
@@ -7,5 +7,8 @@ echo $PASS >> /tmp/.auth
 export PROD_VM_IP=vmkenkins-prod.northeurope.cloudapp.azure.com
 
 scp -i /opt/prod /tmp/.auth prod-user@$PROD_VM_IP:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish.sh prod-user@$PROD_VM_IP:/tmp/publish.sh
-ssh -i /opt/prod prod-user@$PROD_VM_IP "/tmp/publish.sh"
+
+#Uncomment below lines for step - Deploy: Transfer the deployment script to the remote machine
+#scp -i /opt/prod ./jenkins/deploy/publish.sh prod-user@$PROD_VM_IP:/tmp/publish.sh
+#ssh -i /opt/prod prod-user@$PROD_VM_IP "chmod 777 /tmp/publish.sh"
+#ssh -i /opt/prod prod-user@$PROD_VM_IP "/tmp/publish.sh"
