d064f100cbbd23636fd2d22d2cf4d0c9f508645c
Atin Gupta <atingupta2005@gmail.com>
2021-05-16 11:35:27 +0530

    -

--- Hands-On/7-Jenkins-in-Action/Resources/pipeline/jenkins/deploy/deploy.sh
+++ Hands-On/7-Jenkins-in-Action/Resources/pipeline/jenkins/deploy/deploy.sh
@@ -6,6 +6,6 @@ echo $PASS >> /tmp/.auth
 
 export PROD_VM_IP=vmkenkins-prod.northeurope.cloudapp.azure.com
 
-scp -i /opt/prod /tmp/.auth prod-user@l$PROD_VM_IP:/tmp/.auth
+scp -i /opt/prod /tmp/.auth prod-user@$PROD_VM_IP:/tmp/.auth
 scp -i /opt/prod ./jenkins/deploy/publish.sh prod-user@$PROD_VM_IP:/tmp/publish.sh
 ssh -i /opt/prod prod-user@$PROD_VM_IP "/tmp/publish.sh"
